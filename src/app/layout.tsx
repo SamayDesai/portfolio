3ca5bf14963c00166f87f96973cc1012c7e8a829
Samay Desai <samaydesai@gmail.com>
2025-09-16 22:10:11 -0400

    add favicon susan card

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -45,6 +45,7 @@ export default async function RootLayout({
       )}
     >
       <head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
         <script
           id="theme-init"
           dangerouslySetInnerHTML={{
